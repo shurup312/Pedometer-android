1868bc77fe526117a22650a30decc84d9670fc06
shurup312 <bdelphi@mail.ru>
2014-08-30 14:39:16 +0400

    Почистили лишний код

---
+++ app/src/main/java/com/shurup/myapplication/shurup3/app/SensorData.java
@@ -0,0 +1,9 @@
+package com.shurup.myapplication.shurup3.app;
+
+public class SensorData {
+    float[] rotationMatrix; // матрица поворота
+    float[] accelData;      // данные с акселерометра
+    float[] magnetData;     // данные геомагнитного датчика
+    float[] orientationData;// амтрица положения в пространстве
+
+}
